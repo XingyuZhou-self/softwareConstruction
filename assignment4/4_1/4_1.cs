7ea318c96384e39a97f75237d1bf1f49d5d21eac
Xingyu Zhou <3585660562@qq.com>
2025-03-15 15:37:58 +0800

    提交第四次作业，同时修改.gitignore文件，避免提交编译后的文件和相关配置后的文件

---
+++ assignment4/4_1/4_1.cs
@@ -0,0 +1,86 @@
+﻿// 1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+// 通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。
+using System;
+
+namespace GenericLinkedListDemo
+{
+    // 节点类 
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    // 泛型链表类
+    public class MyLinkedList<T>
+    {
+        private Node<T> head;
+
+        // 添加元素到链表尾部
+        public void Add(T value)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(value);
+            }
+            else
+            {
+                Node<T> current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = new Node<T>(value);
+            }
+        }
+
+        // ForEach方法，遍历链表中的所有元素并执行指定的Action
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Value);
+                current = current.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建链表并添加一些整数元素
+            MyLinkedList<int> list = new MyLinkedList<int>();
+            list.Add(5);
+            list.Add(4);
+            list.Add(3);
+            list.Add(8);
+
+            // 打印链表元素
+            Console.WriteLine("链表元素：");
+            list.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            // 使用lambda表达式求最大值、最小值和求和
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+            list.ForEach(x =>
+            {
+                if (x > max) max = x;
+                if (x < min) min = x;
+                sum += x;
+            });
+
+            Console.WriteLine($"最大值：{max}");
+            Console.WriteLine($"最小值：{min}");
+            Console.WriteLine($"求和：{sum}");
+        }
+    }
+}
---
+++ assignment4/4_2/4_2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace AlarmClockDemo
+{
+    // 定义闹钟类
+    public class AlarmClock
+    {
+        // 定义嘀嗒事件
+        public event EventHandler Tick;
+        // 定义响铃事件
+        public event EventHandler Alarm;
+
+        private int tickCount = 0;
+        private int alarmTickCount; // 到达指定嘀嗒次数后触发响铃
+
+        // 构造函数，传入闹钟响铃前需要嘀嗒的次数
+        public AlarmClock(int alarmTickCount)
+        {
+            this.alarmTickCount = alarmTickCount;
+        }
+
+        // 模拟闹钟运行
+        public void Start()
+        {
+            while (tickCount < alarmTickCount)
+            {
+                Thread.Sleep(1000); // 模拟1秒钟的时间流逝
+                tickCount++;
+                // 触发嘀嗒事件
+                Tick?.Invoke(this, EventArgs.Empty);
+            }
+            // 达到指定次数后，触发响铃事件
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建一个闹钟实例，设定5次嘀嗒后响铃
+            AlarmClock alarmClock = new AlarmClock(5);
+
+            // 订阅嘀嗒事件，输出提示信息
+            alarmClock.Tick += (sender, e) =>
+            {
+                Console.WriteLine("嘀嗒...");
+            };
+
+            // 订阅响铃事件，输出提示信息
+            alarmClock.Alarm += (sender, e) =>
+            {
+                Console.WriteLine("响铃：闹钟响了！");
+            };
+
+            Console.WriteLine("闹钟启动...");
+            alarmClock.Start();
+            Console.WriteLine("闹钟停止.");
+        }
+    }
+}
