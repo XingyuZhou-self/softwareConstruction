f0fa9a1dcef87e05794dbf62304a5956d0aede31
Xingyu Zhou <3585660562@qq.com>
2025-04-17 11:29:48 +0800

    采用老师提供的数据库版本，在此基础上进行web api开发

---
+++ assignment8/OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using OrderContracts;
+
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            var orders = _orderService.GetOrders();
+            return Ok(orders);
+        }
+    }
+}
